72eba9a51e642cf58fa3266896b5e4764d95eeaf
Vinicius Godoy Ferreira <viniciusgodoyf@gmail.com>
2021-11-29 20:26:12 -0300

    Prepara o front-end para consumir o Microsserviço de Produtos

--- GeekShopping/GeekShopping.Web/Program.cs
+++ GeekShopping/GeekShopping.Web/Program.cs
@@ -1,8 +1,11 @@
+using GeekShopping.Web.Services;
+using GeekShopping.Web.Services.IServices;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
+builder.Services.AddHttpClient<IProductService, ProductService>(op => op.BaseAddress = new Uri("http://localhost:5021"));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
---
+++ GeekShopping/GeekShopping.Web/Utils/HttpClientExtensions.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+using System.Net.Http.Headers;
+
+namespace GeekShopping.Web.Utils
+{
+    public static class HttpClientExtensions
+    {
+        private static MediaTypeHeaderValue contentType = new MediaTypeHeaderValue("application/json");
+        public static async Task<T> ReadContentAs<T>(this HttpResponseMessage response)
+        {
+            if (!response.IsSuccessStatusCode)
+                throw new ApplicationException($"Something went wrong calling the API: {response.ReasonPhrase}");
+
+            var dataAsString = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+            return JsonConvert.DeserializeObject<T>(dataAsString);
+        }
+
+        public static Task<HttpResponseMessage> PostAsJson<T>(this HttpClient httpClient, string url, T data)
+        {
+            var dataAsString = JsonConvert.SerializeObject(data);
+            var content = new StringContent(dataAsString);
+            content.Headers.ContentType = contentType;
+            return httpClient.PostAsync(url, content);
+        }
+
+        public static Task<HttpResponseMessage> PutAsJson<T>(this HttpClient httpClient, string url, T data)
+        {
+            var dataAsString = JsonConvert.SerializeObject(data);
+            var content = new StringContent(dataAsString);
+            content.Headers.ContentType = contentType;
+            return httpClient.PutAsync(url, content);
+        }
+    }
+}
