ae7acc2669e6d6b8cb14876ae9cd5e5536bc729c
Vinicius Godoy Ferreira <viniciusgodoyf@gmail.com>
2021-11-30 15:55:06 -0300

    Consome o microsserviços de produtos através do front-end.

--- GeekShopping/GeekShopping.Web/Controllers/ProductController.cs
+++ GeekShopping/GeekShopping.Web/Controllers/ProductController.cs
@@ -10,7 +10,7 @@ namespace GeekShopping.Web.Controllers
 
         public ProductController(IProductService productService)
         {
-             _productService = productService ?? throw new ArgumentNullException(nameof(productService));
+            _productService = productService ?? throw new ArgumentNullException(nameof(productService));
         }
 
         public async Task<IActionResult> ProductIndex()
@@ -34,6 +34,49 @@ namespace GeekShopping.Web.Controllers
                 if (response != null)
                     return RedirectToAction(nameof(ProductIndex));
             }
+
+            return View(model);
+        }
+
+        public async Task<IActionResult> ProductUpdate(int id)
+        {
+            var product = await _productService.FindProductById(id);
+            if (product != null)
+                return View(product);
+            else
+                return NotFound();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ProductUpdate(ProductModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var response = await _productService.UpdateProduct(model);
+
+                if (response != null)
+                    return RedirectToAction(nameof(ProductIndex));
+            }
+
+            return View(model);
+        }
+
+        public async Task<IActionResult> ProductDelete(int id)
+        {
+            var product = await _productService.FindProductById(id);
+            if (product != null)
+                return View(product);
+            else
+                return NotFound();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ProductDelete(ProductModel model)
+        {
+            var response = await _productService.DeleteProductById(model.Id);
+
+            if (response)
+                return RedirectToAction(nameof(ProductIndex));
 
             return View(model);
         }
