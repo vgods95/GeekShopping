fe390e69f4a5d8142a152df9622a1c31d704ae04
Vinicius Godoy Ferreira <viniciusgodoyf@gmail.com>
2021-12-08 21:06:23 -0300

    Implementa o microsserviço de carrinho de compras

--- GeekShopping/GeekShopping.CartAPI/Program.cs
+++ GeekShopping/GeekShopping.CartAPI/Program.cs
@@ -1,6 +1,7 @@
 using AutoMapper;
 using GeekShopping.CartAPI.Config;
 using GeekShopping.CartAPI.Model.Context;
+using GeekShopping.CartAPI.Repository;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
 
@@ -73,6 +74,7 @@ builder.Services.AddDbContext<MySQLContext>(op => op
 IMapper mapper = MappingConfig.RegisterMaps().CreateMapper();
 builder.Services.AddSingleton(mapper);
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+builder.Services.AddScoped<ICartRepository, CartRepository>();
 
 var app = builder.Build();
 
