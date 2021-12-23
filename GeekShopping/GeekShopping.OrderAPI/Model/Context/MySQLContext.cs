b7bd353c3095214364d813581053ed0bd59c5782
Vinicius Godoy Ferreira <viniciusgodoyf@gmail.com>
2021-12-23 09:46:59 -0300

    Cria o microsserviço OrderAPI

---
+++ GeekShopping/GeekShopping.OrderAPI/Model/Context/MySQLContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GeekShopping.OrderAPI.Model.Context
+{
+    public class MySQLContext : DbContext
+    {
+        public MySQLContext()
+        { }
+
+        public MySQLContext(DbContextOptions<MySQLContext> options) : base(options)
+        { }
+
+        public DbSet<OrderHeader> OrderHeader { get; set; }
+        public DbSet<OrderDetail> OrderDetail { get; set; }
+
+    }
+}
---
+++ GeekShopping/GeekShopping.OrderAPI/Model/OrderHeader.cs
@@ -0,0 +1,58 @@
+﻿using GeekShopping.OrderAPI.Model.Base;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GeekShopping.OrderAPI.Model
+{
+    [Table("order_header")]
+    public class OrderHeader : BaseEntity
+    {
+        [Column("user_id")]
+        public string UserId { get; set; }
+
+        [Column("coupon_code")]
+        public string? CouponCode { get; set; }
+
+        [Column("purchase_amount")]
+        public decimal PurchaseAmount { get; set; }
+
+        [Column("discount_total")]
+        public decimal DiscountTotal { get; set; }
+
+        [Column("first_name")]
+        public string FirstName { get; set; }
+
+        [Column("last_name")]
+        public string LastName { get; set; }
+
+        [Column("purchase_date")]
+        public DateTime PurchaseDate { get; set; }
+
+        [Column("order_time")]
+        public DateTime OrderTime { get; set; }
+
+        [Column("phone_number")]
+        public string Phone { get; set; }
+
+        [Column("email")]
+        public string Email { get; set; }
+
+        [Column("card_number")]
+        public string CardNumber { get; set; }
+
+        [Column("cvv")]
+        public string CVV { get; set; }
+
+        [Column("expire_month_year")]
+        public string ExpireMonthYear { get; set; }
+
+        [Column("order_total_items")]
+        public int OrderTotalItems { get; set; }
+
+        [ValidateNever]
+        public IEnumerable<OrderDetail> OrderDetail { get; set; }
+
+        [Column("payment_status")]
+        public bool PaymentStatus { get; set; }
+    }
+}
